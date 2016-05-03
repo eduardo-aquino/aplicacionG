b64578d35b6e9abb382a636896e7cbade5e7dbc9
eduardo-aquino <eduardoaquino4g.dam@gmail.com>
2016-05-03 11:10:35 -0500

    2subida

--- js/acciones.js
+++ js/acciones.js
@@ -4,11 +4,11 @@ $(document).ready(function (e){
   document.addEventListener("deviceready",function(){
 	  
 	$('#beep').tap(function(){
-	navigator .notification.beep (1);
+	navigator .notification.beep (2);
 	});//tap beep
 	
 	$('#vibrar').tap(function(){
-    navigator .notification.beep (1);
+    navigator .notification.beep (1000);
 	});//tap vibrar
 	
   },false);//deviceready
